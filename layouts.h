46ca18d3f75ae36f680b9aba7d721c9c4bb32859
Cem Aksoylar <caksoylar@gmail.com>
2022-06-03 16:27:56 -0700

    Move symbols around

--- layouts.h
+++ layouts.h
@@ -83,7 +83,7 @@
     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_BSLS, KC_7,    KC_8,    KC_9,    KC_COMM, _______, \
     KC_LBRC, KC_PLUS, KC_EQL,  KC_UNDS, KC_MINS, KC_CIRC, KC_0,    KC_4,    KC_5,    KC_6,    KC_DOT,  KC_RBRC, \
-    _______, XXXXXXX, INV_QM,  KC_BSLS, KC_ASTR, KC_AMPR, KC_SCLN, KC_1,    KC_2,    KC_3,    KC_SLSH, _______, \
+    _______, XXXXXXX, XXXXXXX, INV_QM,  KC_ASTR, KC_AMPR, KC_SCLN, KC_1,    KC_2,    KC_3,    KC_SLSH, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 
 #define _NAV \
