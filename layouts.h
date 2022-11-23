3df4a2b80ad033fe7df209d79776fd3eb39ba1f2
Cem Aksoylar <caksoylar@gmail.com>
2022-11-22 23:53:00 -0800

    Add space to SYM layer

--- layouts.h
+++ layouts.h
@@ -84,14 +84,14 @@
     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_BSLS, KC_7,    KC_8,    KC_9,    KC_COMM, _______, \
     KC_LBRC, KC_PLUS, KC_EQL,  KC_UNDS, KC_MINS, KC_CIRC, KC_0,    KC_4,    KC_5,    KC_6,    KC_DOT,  KC_RBRC, \
     _______, XXXXXXX, XXXXXXX, INV_QM,  KC_ASTR, KC_AMPR, KC_SCLN, KC_1,    KC_2,    KC_3,    KC_SLSH, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______, _______, _______
 
 #define _NAV \
     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
     _______, ALT_F4,  TAB_LT,  TAB_RT,  CTL_F4,  KC_VOLU, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, _______, \
     _______, KC_LCTL, KC_LGUI, KC_LALT, SFT_TAB, KC_VOLD, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CALC, _______, \
     _______, XXXXXXX, WIN_LT,  WIN_RT,  SFT_INS, KC_MPLY, KC_INS,  KC_DEL,  KC_MPRV, KC_MNXT, XXXXXXX, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, _______, KC_LSFT, _______, _______, _______, _______
 
 #define _FUN \
     _______, DF(DEF), DF(CLM), DF(GME), _______, _______, _______, _______, _______, RGB_VAD, RGB_VAI, RGB_TOG, \
