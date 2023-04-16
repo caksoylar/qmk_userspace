4a9219db66cf2f3bcbe308e949a181d2da31d262
Cem Aksoylar <caksoylar@users.noreply.github.com>
2023-04-15 19:07:22 -0700

    Slower mouse wheel

--- config.h
+++ config.h
@@ -47,15 +47,15 @@
 #endif
 
 #ifdef MOUSEKEY_ENABLE
-#    define MOUSEKEY_DELAY              0
-#    define MOUSEKEY_INTERVAL           8
-#    define MOUSEKEY_MOVE_DELTA         6
-#    define MOUSEKEY_MAX_SPEED         10
-#    define MOUSEKEY_TIME_TO_MAX      160
-#    define MOUSEKEY_WHEEL_DELAY       16
-#    define MOUSEKEY_WHEEL_INTERVAL    40
-#    define MOUSEKEY_WHEEL_MAX_SPEED    8
-#    define MOUSEKEY_WHEEL_TIME_TO_MAX 80
+#    define MOUSEKEY_DELAY               0
+#    define MOUSEKEY_INTERVAL            8
+#    define MOUSEKEY_MOVE_DELTA          6
+#    define MOUSEKEY_MAX_SPEED          10
+#    define MOUSEKEY_TIME_TO_MAX       160
+#    define MOUSEKEY_WHEEL_DELAY        16
+#    define MOUSEKEY_WHEEL_INTERVAL     40
+#    define MOUSEKEY_WHEEL_MAX_SPEED     4
+#    define MOUSEKEY_WHEEL_TIME_TO_MAX 160
 #endif
 
 #include "layouts.h"
