3169ff9751b6e3aeccaf813badc29ba1cf13c97f
Cem Aksoylar <caksoylar@users.noreply.github.com>
2023-10-26 21:52:37 -0700

    Use ffkb tucky thumbs

--- layouts.h
+++ layouts.h
@@ -168,7 +168,7 @@
     XXXXXXX, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, XXXXXXX, \
     XXXXXXX, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, XXXXXXX, \
     XXXXXXX, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, XXXXXXX, \
-    XXXXXXX,           k43, k44, k45, k46, k47, k48,           XXXXXXX, \
+    XXXXXXX,           k44, k45, k43, k48, k46, k47,           XXXXXXX, \
                                   XXXXXXX
 
 #define C_FFKB_GAMING(k) L_5x12_TO_FFKB_GAMING(k)
